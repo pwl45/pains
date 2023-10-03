845421e44e6cf19eb2c7718e4de25e123b9bb4a5
Paul <plapey45@gmail.com>
2023-10-03 18:59:46 -0400

    Update take command line arguments

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,7 @@ fn coalesce_maps<K, V, E>(
     dest: &mut HashMap<K, Result<V, E>>,
     src: HashMap<K, Result<V, E>>,
 ) where
-    K: std::cmp::Eq + std::hash::Hash + Clone,
+K: std::cmp::Eq + std::hash::Hash + Clone,
     // V: Clone,
     // E: Clone,
 {
@@ -114,14 +114,11 @@ fn coalesce_maps<K, V, E>(
 }
 
 fn get_document(url:&str) -> Result<scraper::Html,StockScrapeError>{
-
     let client = reqwest::blocking::Client::new();
-    println!("{}",url);
     let response=client.get(url).header(header::USER_AGENT,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36").send()
         .map_err(StockScrapeError::FetchError)?
         .text()
         .map_err(StockScrapeError::ReadError)?;
-
     return Ok(scraper::Html::parse_document(&response));
 }
 
@@ -133,19 +130,19 @@ fn get_attr_multiple_selector<F>(
 ) -> Result<String, StockScrapeError>
 where
     F: FnOnce(&str) -> String,
-{
-    for selector in price_selectors {
-        let title_selector = scraper::Selector::parse(selector)
-            .map_err(|_| StockScrapeError::SelectorParseError)?;
+    {
+        for selector in price_selectors {
+            let title_selector = scraper::Selector::parse(selector)
+                .map_err(|_| StockScrapeError::SelectorParseError)?;
 
-        if let Some(element) = document.select(&title_selector).next() {
-            let result = element.text().collect::<String>();
-            return Ok(price_selectors_transformer(&result));
+            if let Some(element) = document.select(&title_selector).next() {
+                let result = element.text().collect::<String>();
+                return Ok(price_selectors_transformer(&result));
+            }
+
         }
-
+        Err(StockScrapeError::NoMatchError)
     }
-    Err(StockScrapeError::NoMatchError)
-}
 
 fn get_all_attrs_robust(s: &Stock, sources: &Vec<StockSource>) -> HashMap<AttrID, Result<String, StockScrapeError>> {
     let all_attrs = AttrID::iter().collect();
@@ -155,33 +152,38 @@ fn get_all_attrs_robust(s: &Stock, sources: &Vec<StockSource>) -> HashMap<AttrID
 fn get_attrs_robust(s: &Stock, sources: &Vec<StockSource>, attrs: &Vec<AttrID>) 
     -> HashMap<AttrID, Result<String, StockScrapeError>> {
 
-    // let mut agg_results: HashMap<AttrID, Result<String, StockScrapeError>> = HashMap::new();
         let mut agg_results: HashMap<AttrID, Result<String, StockScrapeError>> = attrs.iter().cloned()
             .map(|attr| (attr, Err(StockScrapeError::NoMatchError)))
             .collect();
-
-    // do random
-    let mut indices: Vec<usize> = (0..sources.len()).collect();
-    indices.shuffle(&mut rand::thread_rng());
-    for i in indices{
-        println!("{}",i);
-        let source = &sources[i];
-        match get_attrs_source(s, source, attrs) {
-            Ok(source_map) => {
-                coalesce_maps(&mut agg_results,source_map);
-            },
-
-            Err(_) => continue,
+        // do random
+        let mut indices: Vec<usize> = (0..sources.len()).collect();
+        indices.shuffle(&mut rand::thread_rng());
+        for i in indices{
+            //Check if every key in attrs maps to an OK value in agg_results. If so, return agg_results. Otherwise, continue
+            if attrs.iter().all(|attr| {
+                match agg_results.get(attr) {
+                    Some(Ok(_)) => true,
+                    _ => false,
+                }
+            }) {
+                return agg_results;
+            }
+            let source = &sources[i];
+            match get_attrs_source(s, source, attrs) {
+                Ok(source_map) => {
+                    coalesce_maps(&mut agg_results,source_map);
+                },
+                Err(_) => continue,
+            }
+            // println!("{:?}",agg_results);
         }
-    }
-    return agg_results;
-
-    // Err(StockScrapeError::NoMatchError)
+        return agg_results;
 }
 
 
 fn get_attrs_source(s: &Stock, so: &StockSource, attrs: &Vec<AttrID>) -> Result<HashMap<AttrID, Result<String, StockScrapeError>>, StockScrapeError> {
     let full_url = (so.url_transformer)(&so.base_url, &s)?;
+    // println!("{}",full_url);
     let document = get_document(&full_url)?;
 
     let mut results = HashMap::new();
@@ -198,7 +200,7 @@ fn get_attrs_source(s: &Stock, so: &StockSource, attrs: &Vec<AttrID>) -> Result<
 
     Ok(results)
 }
-fn test_stonk(sources: &mut Vec<StockSource>){
+fn test_stonk(sources: &Vec<StockSource>){
     let stocks = vec![
         Stock::new("ROIV","NASDAQ"),
         Stock::new("GOOG","NASDAQ"),
@@ -209,14 +211,29 @@ fn test_stonk(sources: &mut Vec<StockSource>){
     ];
 
     for s in &stocks {
-        // sources.shuffle(&mut rng);
         let result_dict=get_all_attrs_robust(s,sources);
         println!("{:?}",&result_dict);
     }
 }
 
+fn get_quote(s: &Stock, sources: &Vec<StockSource>)-> String{
+    let attrs = vec![AttrID::Price, AttrID::PctCh];
+    let result_dict: HashMap<AttrID, Result<String, StockScrapeError>>  = get_attrs_robust(s,sources,&attrs);
+    let price = match result_dict.get(&AttrID::Price) {
+        Some(Ok(price)) => price.clone(),
+        Some(Err(_)) => "?".to_string(),
+        None => "?".to_string(),
+    };
+    let pctch = match result_dict.get(&AttrID::PctCh) {
+        Some(Ok(price)) => price.clone(),
+        Some(Err(_)) => "?".to_string(),
+        None => "?".to_string(),
+    };
+    return format!("{}: {} ({})",s.ticker,price,pctch);
+}
+
 fn main() {
-    let mut sources = vec![
+    let sources = vec![
         StockSource {
             id: SourceID::Yahoo,
             base_url: "https://finance.yahoo.com/quote/".to_string(),
@@ -241,15 +258,14 @@ fn main() {
                         let re = Regex::new(r".*\((.*)\)").unwrap();
                         if let Some(captured) = re.captures(result) {
                             if let Some(matched) = captured.get(1) {
-                                // println!("{}", matched.as_str());
                                 return matched.as_str().to_string();
                             }
                         }
                         return "".to_string();
                     },
                 },
-            ],
-            needs_exchange: false,
+                ],
+                needs_exchange: false,
         },
         StockSource {
             id: SourceID::SeekingAlpha,
@@ -258,7 +274,7 @@ fn main() {
             attributes: hashmap![
                 AttrID::Price => AttrGettr {
                     attr_id: AttrID::Price,
-                    get_selectors: |stock: &Stock| {
+                    get_selectors: |_stock: &Stock| {
                         vec![r#"[data-test-id="symbol-price"]"#.to_string()]
                     },
                     result_transformer: |result: &str| {
@@ -280,12 +296,60 @@ fn main() {
                         return "".to_string();
                     },
                 },
-            ],
-            needs_exchange: false,
+                ],
+                needs_exchange: false,
         },
-    ];
+        StockSource {
+            id: SourceID::GoogleFinance,
+           base_url: "https://www.google.com/finance/quote/".to_string(),
+           url_transformer: |base_url, stock| {
+               let exchange_str= stock.exchange.as_ref().ok_or(StockScrapeError::NoExchangeError)?;
+               Ok(format!("{}{}:{}", base_url, stock.ticker,exchange_str))
+           },
+            attributes: hashmap![
+                AttrID::Price => AttrGettr {
+                    attr_id: AttrID::Price,
+                    get_selectors: |_stock: &Stock| {
+                        vec!["div.YMlKec.fxKbKc".to_string()]
+                    },
+                    result_transformer: |result: &str| {
+                        result.replace("$","")
+                    },
+                },
+                AttrID::PctCh => AttrGettr {
+                    attr_id: AttrID::PctCh,
+                    get_selectors: |_stock: &Stock| {
+                        vec![r#"div.yWOrNb span[jsname="Fe7oBc"].NydbP div.JwB6zf"#.to_string()]
+                    },
+                    result_transformer: |result: &str| {
+                        // let re = Regex::new(r".*\((.*)\)").unwrap();
+                        // if let Some(captured) = re.captures(result) {
+                        //     if let Some(matched) = captured.get(1) {
+                        //         return matched.as_str().to_string();
+                        //     }
+                        // }
+                        return result.to_string();
+                    },
+                },
+                ],
+                needs_exchange: false,
+        },
+        ];
 
-    test_stonk(&mut sources);
+    // test_stonk(&sources);
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        for arg in &args[1..]{
+            let parts: Vec<&str> = arg.split(':').collect();
+            let ticker = parts[0].trim().to_string();
+            let exchange = parts.get(1).map(|s| s.trim().to_string());
+            println!("{}",get_quote(&Stock{ticker: ticker,exchange: exchange},&sources));
+        }
+    } else {
+        println!("No arguments were provided");
+        std::process::exit(1);
+    }
 
 
 }
